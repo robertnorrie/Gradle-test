57c0c95f6eeebfe9e9a33a4b5a86748db1920c9d
Robert Norrie <robertnorrie@me.com>
2024-06-27 08:41:59 +0100

    changes to version

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ repositories {
 val modelsProject = project(":lib:models")
 dependencies {
     implementation(modelsProject)
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.7.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.6.1")
 }
 
 spotless {
